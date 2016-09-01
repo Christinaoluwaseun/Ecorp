dca260db71ef15bbdf4d75930f5510c34e2a775f
Christinaoluwaseun <christinaoxygen@gmail.com>
2016-09-01 13:49:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dell/academytutorialapp/Ecorp.java
@@ -0,0 +1,13 @@
+package com.example.dell.academytutorialapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ecorp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ecorp);
+    }
+}
